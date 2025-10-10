2e4931730f66295f8d87ea064e85051942381f13
Qi Liu <liuqi050628@163.com>
2025-10-10 22:57:32 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -39,16 +39,16 @@ module.exports = {
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',  //https://music.163.com/song/media/outer/url?id=731419.mp3
+      url: 'http://m3.130v.com:9888/qyy_mp3/6638.mp3',  //https://music.163.com/song/media/outer/url?id=731419.mp3
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
     {
-      name: '王都グランセル', //水星领航员
+      name: '水星领航员', // 原版:王都グランセル
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',  //https://music.163.com/song/media/outer/url?id=731355.mp3
+      url: 'http://m3.130v.com:9888/qyy_mp3/8015.mp3',  //https://music.163.com/song/media/outer/url?id=731355.mp3
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://www.999ttt.com/tp/8014.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || true, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
